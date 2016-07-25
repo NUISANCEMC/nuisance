14da051200a6fd827c37066691612817711127d6
Luke Pickering <lp208@ic.ac.uk>
2016-07-25 04:33:15 +0100

    Adds randomly 'synthesised' library load message for FitBase.

--- src/FitBase/BaseFitEvt.cxx
+++ src/FitBase/BaseFitEvt.cxx
@@ -17,7 +17,88 @@
 *    along with NuFiX.  If not, see <http://www.gnu.org/licenses/>.
 *******************************************************************************/
 
+
+#include <ctime>
+#include <cstdlib>
+#include <cmath>
+
+#include <sstream>
+
+#include "TRandom3.h"
+
 #include "BaseFitEvt.h"
+
+
+struct LetterBackronym {
+  LetterBackronym(size_t n, std::string const &b, float p=1.0, std::string const &t=""){
+    NUsed = n;
+    Backkie = b;
+    ProbAccept = p;
+    TagLine = t;
+  };
+  size_t NUsed;
+  float ProbAccept;
+  std::string Backkie;
+  std::string TagLine;
+};
+
+__attribute__((constructor)) void nuisance_init(void) {
+
+  std::vector< std::vector<LetterBackronym> > Letters;
+
+  for(size_t i = 0; i < 8; ++i){
+    Letters.push_back(std::vector<LetterBackronym>());
+  }
+
+  Letters[0].push_back(LetterBackronym(2,"Neutrino"));
+  Letters[0].push_back(LetterBackronym(3,"NUIsance", 0.2));
+
+  Letters[2].push_back(LetterBackronym(1,"Interaction"));
+
+  Letters[3].push_back(LetterBackronym(1,"Systematics"));
+  Letters[3].push_back(LetterBackronym(1,"Synthesiser", 0.2, "Playing on the comparisons you want to see"));
+
+
+  Letters[4].push_back(LetterBackronym(2,"ANalyser"));
+  Letters[4].push_back(LetterBackronym(1,"Aggregating", 0.5));
+  Letters[4].push_back(LetterBackronym(3,"from A-Neutrino sCattering", 1, "You can always find a frame"));
+
+  Letters[5].push_back(LetterBackronym(1,"New", 1, "The freshest comparisons"));
+
+  Letters[6].push_back(LetterBackronym(1,"by Comparing"));
+  Letters[6].push_back(LetterBackronym(1,"Constraints from"));
+
+  Letters[7].push_back(LetterBackronym(1,"Experiments"));
+
+
+  std::vector<std::string> TagLines;
+  TagLines.push_back("Fit and compare.");
+
+
+  std::stringstream back("");
+
+  TRandom3 tr;
+  tr.SetSeed();
+
+  for(size_t i = 0; i < 8;){
+    LetterBackronym const& let = Letters[i][tr.Integer(Letters[i].size())];
+    if(tr.Uniform() > let.ProbAccept){
+      continue;
+    }
+    back << let.Backkie << " ";
+    i += let.NUsed;
+    if(let.TagLine.length()){
+      TagLines.push_back(let.TagLine);
+    }
+  }
+  std::cout <<
+"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+"%%" << std::endl
+  << "%%  Welcome to Nuisance: " << back.str() << "-- "
+  << TagLines[tr.Integer(TagLines.size())] << std::endl <<
+"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+"%%" << std::endl;
+}
 
 BaseFitEvt::BaseFitEvt(){
 
@@ -80,4 +161,3 @@ BaseFitEvt::BaseFitEvt(const BaseFitEvt* obj){
     }
   }
 };
-
