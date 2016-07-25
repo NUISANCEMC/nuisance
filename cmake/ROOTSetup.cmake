bddbd96562870186d73380bba1aa707913a30dbf
Luke Pickering <lp208@ic.ac.uk>
2016-07-25 09:04:17 +0100

    Adds support for non-minimizer build. Apps are now built based upon make options.

--- cmake/ROOTSetup.cmake
+++ cmake/ROOTSetup.cmake
@@ -5,23 +5,25 @@ else()
   set(CMAKE_ROOTSYS $ENV{ROOTSYS})
 endif()
 
-#If you want to try an use the terminally buggy ROOT CMake scripts
-if (DEFINED USEROOTCMAKE AND USEROOTCMAKE)
-  include ($ENV{ROOTSYS}/etc/cmake/FindROOT.cmake)
-  if ( NOT ROOT_FOUND )
-    cmessage (FATAL_ERROR "[ROOT]: FindROOT.cmake could not be found, or did not execute as expected is ROOT installed in $ROOTSYS = $ENV{ROOTSYS}?")
-  endif()
-else()
-  # cmessage(STATUS "Including local GENERATE_ROOT_DICTIONARY implementation.")
-  # include(${CMAKE_SOURCE_DIR}/cmake/GenROOTDictionary.cmake)
-endif()
-
 execute_process (COMMAND root-config
   --cflags OUTPUT_VARIABLE ROOT_CXX_FLAGS OUTPUT_STRIP_TRAILING_WHITESPACE)
 execute_process (COMMAND root-config
   --evelibs OUTPUT_VARIABLE ROOT_LD_FLAGS OUTPUT_STRIP_TRAILING_WHITESPACE)
 execute_process (COMMAND root-config
   --version OUTPUT_VARIABLE ROOT_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+execute_process (COMMAND root-config
+  --features OUTPUT_VARIABLE ROOT_FEATURES OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+
+if(NOT DEFINED USE_MINIMIZER)
+  if("${ROOT_FEATURES}" MATCHES "minuit2")
+    cmessage(STATUS "ROOT built with MINUIT2 support")
+    set(USE_MINIMIZER 1)
+  else()
+    cmessage(STATUS "ROOT built without MINUIT2 support, minimizer functionality will be disabled.")
+    set(USE_MINIMIZER 0)
+  endif()
+endif()
 
 if(DEFINED NEED_ROOTPYTHIA6 AND NEED_ROOTPYTHIA6)
   set(ROOT_LD_FLAGS "${ROOT_LD_FLAGS} -lEGPythia6 ")
--- cmake/pythia6Setup.cmake
+++ cmake/pythia6Setup.cmake
@@ -1,14 +1,18 @@
 if(NOT DEFINED SET_UP_PYTHIA6)
 
   if(DEFINED NEED_PYTHIA6 AND NEED_PYTHIA6)
-    if(NOT DEFINED ENV{PYTHIA6})
+    if(NOT DEFINED ENV{PYTHIA6} AND NOT DEFINED ENV{PYTHIA6_LIB})
 
-      cmessage(FATAL_ERROR "Environment variable PYTHIA6 is not defined. "
+      cmessage(FATAL_ERROR "Environment variable PYTHIA6/PYTHIA6_LIB is not defined. "
         "This must be set to point to a prebuilt NuWro instance.")
 
     endif()
 
-    set(PYTHIA6 $ENV{PYTHIA6})
+    if(DEFINED ENV{PYTHIA6})
+      set(PYTHIA6 $ENV{PYTHIA6})
+    elseif(DEFINED ENV{PYTHIA6_LIB})
+      set(PYTHIA6 $ENV{PYTHIA6_LIB})
+    endif()
     set(CMAKE_LINK_FLAGS "${CMAKE_LINK_FLAGS} -L${PYTHIA6} -lPythia6 -lgfortran")
 
   else()
