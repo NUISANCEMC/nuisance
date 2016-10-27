5c66dd7f803a5f802e774c3a159f73b761383bf9
cwilkinson <callum.wilkinson@lhep.unibe.ch>
2016-10-27 15:01:40 +0200

    Update ArgoNeut to use a generic signal definition.

--- src/ArgoNeuT/ArgoNeuT_CCInc_XSec_1Dthetamu_nu.cxx
+++ src/ArgoNeuT/ArgoNeuT_CCInc_XSec_1Dthetamu_nu.cxx
@@ -38,5 +38,5 @@ void ArgoNeuT_CCInc_XSec_1Dthetamu_nu::FillEventVariables(FitEvent *event) {
 bool ArgoNeuT_CCInc_XSec_1Dthetamu_nu::isSignal(FitEvent *event)
 //********************************************************************
 {
-  return SignalDef::isCCInc_ArgoNeuT(event);
+  return SignalDef::isCCINC(event, 14);
 }
