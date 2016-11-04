fc8ad4f28b526a7b1189be66d0349292a898891c
cwilkinson <callum.wilkinson@lhep.unibe.ch>
2016-11-04 07:52:06 +0100

    Remove repeated logging information

--- src/FitBase/InputHandler.cxx
+++ src/FitBase/InputHandler.cxx
@@ -775,7 +775,6 @@ void InputHandler::ReadGenieFile() {
   tn->AddFile(fInputFile.c_str());
 
   fNEvents = tn->GetEntries();
-  LOG(SAM) << "Number of GENIE Events " << tn->GetEntries() << std::endl;
   StopTalking();
   fGenieGHep = NULL;
   fGenieNtpl = NULL;
