995a7c21f33fcc158bdad0ea2af0cab80b60a4cd
cwilkinson <callum.wilkinson@lhep.unibe.ch>
2016-11-09 08:40:01 +0100

    Fix typos that caused samples to fail.

--- src/ANL/ANL_CC1pi0_Evt_1DQ2_nu.cxx
+++ src/ANL/ANL_CC1pi0_Evt_1DQ2_nu.cxx
@@ -51,7 +51,7 @@ ANL_CC1pi0_Evt_1DQ2_nu::ANL_CC1pi0_Evt_1DQ2_nu(std::string inputfile, FitWeight 
 void ANL_CC1pi0_Evt_1DQ2_nu::FillEventVariables(FitEvent *event) {
 
   if (event->NumFSParticle(2212) == 0 ||
-      event->NumFSParticle(211) == 0 ||
+      event->NumFSParticle(111) == 0 ||
       event->NumFSParticle(13) == 0)
     return;
 
