31be0801cd209c1a943415535587bbdd108b2c79
Patrick Stowell <p.stowell@sheffield.ac.uk>
2016-12-13 17:01:26 +0000

    Fixed joint binning for MINERvA CCQE

--- src/MINERvA/MINERvA_CCQE_XSec_1DQ2_joint.cxx
+++ src/MINERvA/MINERvA_CCQE_XSec_1DQ2_joint.cxx
@@ -120,7 +120,7 @@ MINERvA_CCQE_XSec_1DQ2_joint::MINERvA_CCQE_XSec_1DQ2_joint(std::string name, std
   this->fSubChain.push_back(MIN_anu);
   this->fSubChain.push_back(MIN_nu);
 
-  this->fFluxHist = GetCombinedFlux();
+  this->fFluxHist  = GetCombinedFlux();
   this->fEventHist = GetCombinedEventRate();
 
   // Setup Default MC Hists
@@ -142,6 +142,7 @@ void MINERvA_CCQE_XSec_1DQ2_joint::MakePlots(){
       MIN_anu = static_cast<MINERvA_CCQE_XSec_1DQ2_antinu*>(exp);
       TH1D* MIN_anu_mc = (TH1D*) MIN_anu->GetMCList().at(0);
       for (int i = 0; i < 8; i++){
+	std::cout << "Adding MIN_anu_MC " << i+1 << " : " << i+1 << " " << MIN_anu_mc->GetBinContent(i+1) << std::endl;
 	fMCHist->SetBinContent(i+1, MIN_anu_mc->GetBinContent(i+1));
 	fMCHist->SetBinError(i+1, MIN_anu_mc->GetBinError(i+1));
       }
@@ -150,10 +151,12 @@ void MINERvA_CCQE_XSec_1DQ2_joint::MakePlots(){
       MIN_nu = static_cast<MINERvA_CCQE_XSec_1DQ2_nu*>(exp);
       TH1D* MIN_nu_mc = (TH1D*) MIN_nu->GetMCList().at(0);
       for (int i = 0; i < 8; i++){
+	std::cout << "Adding MIN_nu_MC " << i+1+8 << " : " << i+1 << " " << MIN_nu_mc->GetBinContent(i+1) << std::endl;
 	fMCHist->SetBinContent(i+1+8, MIN_nu_mc->GetBinContent(i+1));
+	fMCHist  ->SetBinError(i+1+8, MIN_nu_mc->GetBinError(i+1));
       }
 
-    } else break;
+    } 
     sample++;
   }
 
