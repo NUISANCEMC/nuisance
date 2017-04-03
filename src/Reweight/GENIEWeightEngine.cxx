c4195d671ac77f559f979afbdf7a121bf2f20e07
Patrick Stowell <p.stowell@sheffield.ac.uk>
2017-04-03 15:42:43 +0100

    Removed debugging statement I missed in GENIEWeightEngine.

--- src/Reweight/GENIEWeightEngine.cxx
+++ src/Reweight/GENIEWeightEngine.cxx
@@ -160,7 +160,7 @@ double GENIEWeightEngine::CalcWeight(BaseFitEvt* evt) {
 
 	// Make nom weight
 	rw_weight = fGenieRW->CalcWeight(*(evt->genie_event->event));
-	std::cout << "Returning GENIE Weight for electron scattering = " << rw_weight << std::endl;
+	//	std::cout << "Returning GENIE Weight for electron scattering = " << rw_weight << std::endl;
 #endif
 
 	// Return rw_weight
