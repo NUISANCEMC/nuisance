26547e0c284b30ae0cf10eddaa8a6b565f7e786e
Patrick Stowell <p.stowell@sheffield.ac.uk>
2017-05-11 10:27:52 +0100

    Added new MINERvA CC0pi1p target datasets. Ratios to come soon. Changed Debugging state to -O3 whilst I run profilling on master. Fixed a scaling issue in the new Modes histogram.

---
+++ src/MINERvA/MINERvA_CC0pi_XSec_1DQ2_Tgt_nu.cxx
@@ -0,0 +1,142 @@
+// Copyright 2016 L. Pickering, P Stowell, R. Terri, C. Wilkinson, C. Wret
+
+/*******************************************************************************
+*    This file is part of NUISANCE.
+*
+*    NUISANCE is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    NUISANCE is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with NUISANCE.  If not, see <http://www.gnu.org/licenses/>.
+*******************************************************************************/
+
+#include <string>
+#include <sstream>
+
+#include "MINERvA_SignalDef.h"
+#include "MINERvA_CC0pi_XSec_1DQ2_Tgt_nu.h"
+
+//********************************************************************
+MINERvA_CC0pi_XSec_1DQ2_Tgt_nu::MINERvA_CC0pi_XSec_1DQ2_Tgt_nu(nuiskey samplekey) {
+//********************************************************************
+
+  // Sample overview ---------------------------------------------------
+  std::string descrip = "MINERvA_CC0pi_XSec_1DQ2_Tgt_nu sample. \n" \
+                        "Target: CH \n" \
+                        "Flux: MINERvA Forward Horn Current nue + nuebar \n" \
+                        "Signal: Any event with 1 electron, any nucleons, and no other FS particles \n";
+
+  // Setup common settings
+  fSettings = LoadSampleSettings(samplekey);
+  fSettings.SetDescription(descrip);
+  fSettings.SetXTitle("Q^{2}_{QE} (GeV^{2})");
+  fSettings.SetYTitle("d#sigma/dQ^{2} (cm^{2}/GeV^{2})");
+  fSettings.SetAllowedTypes("FIX,FREE,SHAPE/DIAG/NORM", "FIX/DIAG");
+  fSettings.SetEnuRange(0.0, 100.0);
+  fSettings.DefineAllowedTargets("C,H");
+
+  // CCQELike plot information
+  fSettings.SetTitle("MINERvA_CC0pi_XSec_1DQ2_Tgt_nu");
+  // no Data right now so we fake it
+  //fSettings.SetDataInput(  FitPar::GetDataBase() + "/MINERvA/CCQE/proton_Q2QE_nu_data.txt" );
+  //fSettings.SetCovarInput( FitPar::GetDataBase() + "/MINERvA/CCQE/proton_Q2QE_nu_covar.txt" );
+  fSettings.DefineAllowedSpecies("numu");
+
+
+  // Set Target From name
+  if (fSettings.Found("name","_CH")){
+    fTarget = 1; // kTargetCH;
+    fSettings.SetDataInput(  FitPar::GetDataBase() + "/MINERvA/CC0pi/Q2_Tgt_CH_data.txt");
+  } else if (fSettings.Found("name","_C")){
+    fTarget = 2; //kTargetC;
+    fSettings.SetDataInput(  FitPar::GetDataBase() + "/MINERvA/CC0pi/Q2_Tgt_C_data.txt");
+    fSettings.SetCovarInput(  FitPar::GetDataBase() + "/MINERvA/CC0pi/Q2_Tgt_C_covar.txt");
+  }  else if (fSettings.Found("name","_Fe")){
+    fTarget = 3; //kTargetFe;
+    fSettings.SetDataInput(  FitPar::GetDataBase() + "/MINERvA/CC0pi/Q2_Tgt_Fe_data.txt");
+    fSettings.SetCovarInput(  FitPar::GetDataBase() + "/MINERvA/CC0pi/Q2_Tgt_Fe_covar.txt");
+  } else if (fSettings.Found("name","_Pb")){
+    fTarget = 4; //kTargetPb;
+    fSettings.SetDataInput(  FitPar::GetDataBase() + "/MINERvA/CC0pi/Q2_Tgt_Pb_data.txt");
+    fSettings.SetCovarInput(  FitPar::GetDataBase() + "/MINERvA/CC0pi/Q2_Tgt_Pb_covar.txt");
+  } else {
+    ERR(FTL) << "Target not found in name! " << std::endl;
+    throw;
+  }
+
+
+  FinaliseSampleSettings();
+
+  // Scaling Setup ---------------------------------------------------
+  // ScaleFactor automatically setup for DiffXSec/cm2/Nucleon
+  fScaleFactor = (GetEventHistogram()->Integral("width")*1E-38/(fNEvents+0.))/TotalIntegratedFlux();
+
+  // Plot Setup -------------------------------------------------------
+  SetDataFromTextFile( fSettings.GetDataInput() );
+  if (fTarget == 1){
+    SetCovarFromDiagonal(fDataHist);
+  } else {
+    SetCorrelationFromTextFile(fSettings.GetCovarInput() );
+  }
+
+  // Final setup  ---------------------------------------------------
+  FinaliseMeasurement();
+
+};
+
+void MINERvA_CC0pi_XSec_1DQ2_Tgt_nu::FillEventVariables(FitEvent *event){
+
+  // Has NuMuCC1p
+  if (event->HasISNuMuon() &&
+      event->HasFSMuon() &&
+      event->HasFSProton()){
+
+    TLorentzVector pnu    = event->GetHMISNuMuon()->fP;
+    TLorentzVector pprot  = event->GetHMFSProton()->fP;
+    TLorentzVector pmu    = event->GetHMFSMuon()->fP;
+
+    // Q2QE rec from leading proton assuming 34 MeV Eb
+    double protmax = pprot.E();
+    double q2qe    = FitUtils::ProtonQ2QErec(protmax, 34.);
+
+    // Coplanar is angle between muon and proton plane
+    TVector3 plnprotnu = pprot.Vect().Cross(pnu.Vect());
+    TVector3 plnmunu   = pmu.Vect().Cross(pnu.Vect());
+    double copl        = plnprotnu.Angle(plnmunu);
+
+    // Fill X Variables
+    fXVar = q2qe;
+
+    // Save Coplanar into spare y variable
+    fYVar = copl;
+  }
+
+  return;
+};
+
+
+bool MINERvA_CC0pi_XSec_1DQ2_Tgt_nu::isSignal(FitEvent *event){
+
+  // IS NuMu + FS Muon
+  if (!event->HasISNuMuon()) return false;
+  if (!event->HasFSMuon()) return false;
+
+  // No Pions
+  
+
+  // Require 1 FS proton
+  if (!event->HasFSProton()) return false;
+
+  // Proton Momentum > 450
+  TLorentzVector pprot  = event->GetHMFSProton()->fP;
+  if (pprot.Vect().Mag() < 450.0) return false;
+
+  return true;
+};
