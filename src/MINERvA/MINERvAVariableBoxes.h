203f38250af4521f5c003a82117bce93af5a9434
patrickstowell <p.stowell@sheffield.ac.uk>
2017-08-15 20:01:01 -0500

    Updated MINERvA Variable Boxes to clone properly so that the Tpi and Thpi distributions work in CCNpip samples when using SignalReconfigures        modified:   src/FitBase/MeasurementBase.cxx        modified:   src/MINERvA/MINERvAVariableBoxes.h        modified:   src/MINERvA/MINERvA_CCNpip_XSec_1DTpi_nu.cxx

--- src/MINERvA/MINERvAVariableBoxes.h
+++ src/MINERvA/MINERvAVariableBoxes.h
@@ -13,16 +13,43 @@
 /// Custom box used to also save All Pion Tpi for each event.
 class NTpiVariableBox1D : public MeasurementVariableBox1D {
 public:
-	inline NTpiVariableBox1D() { Reset(); };
+	inline NTpiVariableBox1D() { };
 	inline void Reset() {  fTpiVect.clear(); } 
+
+    inline MeasurementVariableBox* CloneSignalBox(){
+        NTpiVariableBox1D* box = new NTpiVariableBox1D(); 
+        box->fX = this->fX;
+        box->fSampleWeight = this->fSampleWeight;
+
+        box->fTpiVect.clear();
+        for (int i = 0; i < this->fTpiVect.size(); i++){
+            box->fTpiVect.push_back( this->fTpiVect[i] );
+        }
+        return box;
+    }
+    inline void Print(){
+        std::cout << "Box Print Size : " << this->fTpiVect.size() << std::endl;
+    }
+
 	std::vector<double> fTpiVect;
 };
 
 /// Custom box used to also save All Pion Tpi for each event.
 class NthpiVariableBox1D : public MeasurementVariableBox1D {
  public:
-  inline NthpiVariableBox1D() { Reset(); };
+  inline NthpiVariableBox1D() {  };
   inline void Reset() {  fthpiVect.clear(); }
+  inline MeasurementVariableBox* CloneSignalBox(){
+        NthpiVariableBox1D* box = new NthpiVariableBox1D(); 
+        box->fX = this->fX;
+        box->fSampleWeight = this->fSampleWeight;
+
+        box->fthpiVect.clear();
+        for (int i = 0; i < this->fthpiVect.size(); i++){
+            box->fthpiVect.push_back( this->fthpiVect[i] );
+        }
+        return box;
+    }
   std::vector<double> fthpiVect;
 };
 
