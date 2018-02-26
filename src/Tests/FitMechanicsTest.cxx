581b884f4447761a8f29d3b17b38762d80339e19
Luke Pickering <picker24@msu.edu>
2018-02-26 11:13:35 -0600

    Adds a simple minimizer test and Tweaks some debugging in the core fitter, but no significant changes.
    - Minimizer tests creates 3 dummy events, each of different
    interaction modes and uses them to build some weighted fake data
    the minimizer is then given the normalisation dials for these
    interaction modes and tries to recover the fake data weights
    from a histogram binned in true modes -- i.e. has the exact
    orthogonal freedom to get the answer right. This is a test
    of the interface to the fitter and the error estimation in
    a completely idealised scenario.

---
+++ src/Tests/FitMechanicsTest.cxx
@@ -0,0 +1,36 @@
+#include "DummyMinimizer.h"
+
+int main(){
+
+  double FakeDataError = 1E-4;
+  Config::SetPar("FakeDataError",FakeDataError);
+
+  DummyMinimizer min;
+
+  std::cout << "======Running simple minimizer test======" << std::endl;
+
+  min.Run();
+
+  std::cout << "======Done minimizing, checking output======" << std::endl;
+
+  std::vector<string> pnames;
+  pnames.push_back("mode_1");
+  pnames.push_back("mode_2");
+  pnames.push_back("mode_3");
+
+  std::vector<double> postfitvals;
+  postfitvals.push_back(1);
+  postfitvals.push_back(1.5);
+  postfitvals.push_back(2);
+
+  for(size_t i = 0; i < 3; ++i){
+    std::cout << "\tTest param name: " << pnames[i] << " == " << min.GetParamName(i) << std::endl;
+    assert(pnames[i]==min.GetParamName(i));
+    std::cout << "\tTest param post-fit value : fabs(" << postfitvals[i] << " - " << min.GetParamVal(i) << ") < " << min.GetParamError(i)  << std::endl;
+    assert(fabs(postfitvals[i] - min.GetParamVal(i)) < min.GetParamError(i));
+    std::cout << "\tTest post-fit error " << min.GetParamError(i) << " < " << FakeDataError << std::endl;
+    assert(min.GetParamError(i) < FakeDataError);
+  }
+
+  return 0;
+}
