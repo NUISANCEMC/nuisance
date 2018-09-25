a56fc5bd60975b336ea08c95532031bfdf3fe974
cwret <c.wret14@imperial.ac.uk>
2018-09-25 17:29:09 +0100

    trivial indent

--- src/Reweight/MINERvAWeightCalcs.h
+++ src/Reweight/MINERvAWeightCalcs.h
@@ -24,91 +24,91 @@ class BaseFitEvt;
 
 
 namespace nuisance {
-namespace reweight {
+  namespace reweight {
 
-// MEC Dials
-class MINERvAReWeight_QE : public NUISANCEWeightCalc {
- public:
-  MINERvAReWeight_QE();
-  virtual ~MINERvAReWeight_QE();
+    // MEC Dials
+    class MINERvAReWeight_QE : public NUISANCEWeightCalc {
+      public:
+        MINERvAReWeight_QE();
+        virtual ~MINERvAReWeight_QE();
 
-  double CalcWeight(BaseFitEvt* evt);
-  void SetDialValue(std::string name, double val);
-  void SetDialValue(int rwenum, double val);
-  bool IsHandled(int rwenum);
+        double CalcWeight(BaseFitEvt* evt);
+        void SetDialValue(std::string name, double val);
+        void SetDialValue(int rwenum, double val);
+        bool IsHandled(int rwenum);
 
-  double fTwk_NormCCQE;
-  double fCur_NormCCQE;
-  double fDef_NormCCQE;
-    bool fTweaked;
+        double fTwk_NormCCQE;
+        double fCur_NormCCQE;
+        double fDef_NormCCQE;
+        bool fTweaked;
 
-};
+    };
 
 
-// MEC Dials
-class MINERvAReWeight_MEC : public NUISANCEWeightCalc {
- public:
-  MINERvAReWeight_MEC();
-  virtual ~MINERvAReWeight_MEC();
+    // MEC Dials
+    class MINERvAReWeight_MEC : public NUISANCEWeightCalc {
+      public:
+        MINERvAReWeight_MEC();
+        virtual ~MINERvAReWeight_MEC();
 
-  double CalcWeight(BaseFitEvt* evt);
-  void SetDialValue(std::string name, double val);
-  void SetDialValue(int rwenum, double val);
-  bool IsHandled(int rwenum);
+        double CalcWeight(BaseFitEvt* evt);
+        void SetDialValue(std::string name, double val);
+        void SetDialValue(int rwenum, double val);
+        bool IsHandled(int rwenum);
 
-  double fTwk_NormCCMEC;
-  double fCur_NormCCMEC;
-  double fDef_NormCCMEC;
-    bool fTweaked;
+        double fTwk_NormCCMEC;
+        double fCur_NormCCMEC;
+        double fDef_NormCCMEC;
+        bool fTweaked;
 
-};
+    };
 
-// RES Dials
-class MINERvAReWeight_RES : public NUISANCEWeightCalc {
- public:
-   MINERvAReWeight_RES();
-   virtual ~MINERvAReWeight_RES();
+    // RES Dials
+    class MINERvAReWeight_RES : public NUISANCEWeightCalc {
+      public:
+        MINERvAReWeight_RES();
+        virtual ~MINERvAReWeight_RES();
 
-   double CalcWeight(BaseFitEvt* evt);
-   void SetDialValue(std::string name, double val);
-   void SetDialValue(int rwenum, double val);
-   bool IsHandled(int rwenum);
+        double CalcWeight(BaseFitEvt* evt);
+        void SetDialValue(std::string name, double val);
+        void SetDialValue(int rwenum, double val);
+        bool IsHandled(int rwenum);
 
-   double fTwk_NormCCRES;
-   double fCur_NormCCRES;
-   double fDef_NormCCRES;
-    bool fTweaked;
+        double fTwk_NormCCRES;
+        double fCur_NormCCRES;
+        double fDef_NormCCRES;
+        bool fTweaked;
 
- };
+    };
 
-/// RPA Weight Calculator that applies RPA systematics
-/// to GENIE events. GENIE EVENTS ONLY!
-class RikRPA : public NUISANCEWeightCalc {
-public:
-	RikRPA();
-	~RikRPA();
+    /// RPA Weight Calculator that applies RPA systematics
+    /// to GENIE events. GENIE EVENTS ONLY!
+    class RikRPA : public NUISANCEWeightCalc {
+      public:
+        RikRPA();
+        ~RikRPA();
 
-	double CalcWeight(BaseFitEvt* evt);
-	void SetDialValue(std::string name, double val);
-	void SetDialValue(int rwenum, double val);
-	bool IsHandled(int rwenum);
+        double CalcWeight(BaseFitEvt* evt);
+        void SetDialValue(std::string name, double val);
+        void SetDialValue(int rwenum, double val);
+        bool IsHandled(int rwenum);
 
-	void SetupRPACalculator(int calcenum);
-	int GetRPACalcEnum(int bpdg, int tpdg);
+        void SetupRPACalculator(int calcenum);
+        int GetRPACalcEnum(int bpdg, int tpdg);
 
-	bool   fApplyDial_RPACorrection;
+        bool   fApplyDial_RPACorrection;
 
-	double fTwkDial_RPALowQ2;
-	double fDefDial_RPALowQ2;
-	double fCurDial_RPALowQ2;
-	double fErrDial_RPALowQ2;
+        double fTwkDial_RPALowQ2;
+        double fDefDial_RPALowQ2;
+        double fCurDial_RPALowQ2;
+        double fErrDial_RPALowQ2;
 
-	double fTwkDial_RPAHighQ2;
-	double fDefDial_RPAHighQ2;
-	double fCurDial_RPAHighQ2;
-	double fErrDial_RPAHighQ2;
-	
-	bool   fApplyDial_RESRPACorrection;
+        double fTwkDial_RPAHighQ2;
+        double fDefDial_RPAHighQ2;
+        double fCurDial_RPAHighQ2;
+        double fErrDial_RPAHighQ2;
+
+        bool   fApplyDial_RESRPACorrection;
 
         double fTwkDial_RESRPALowQ2;
         double fDefDial_RESRPALowQ2;
@@ -120,26 +120,26 @@ public:
         double fCurDial_RESRPAHighQ2;
         double fErrDial_RESRPAHighQ2;
 
-	double* fEventWeights;
-	bool fTweaked;
+        double* fEventWeights;
+        bool fTweaked;
 
-	const static int kMaxCalculators = 10;
-	enum rpacalcenums {
-		kNuMuC12,
-		kNuMuO16,
-		kNuMuAr40,
-		kNuMuCa40,
-		kNuMuFe56,
-		kNuMuBarC12,
-		kNuMuBarO16,
-		kNuMuBarAr40,
-		kNuMuBarCa40,
-		kNuMuBarFe56
-	};
-	weightRPA* fRPACalculators[kMaxCalculators];
-};
+        const static int kMaxCalculators = 10;
+        enum rpacalcenums {
+          kNuMuC12,
+          kNuMuO16,
+          kNuMuAr40,
+          kNuMuCa40,
+          kNuMuFe56,
+          kNuMuBarC12,
+          kNuMuBarO16,
+          kNuMuBarAr40,
+          kNuMuBarCa40,
+          kNuMuBarFe56
+        };
+        weightRPA* fRPACalculators[kMaxCalculators];
+    };
 
-};  // namespace reweight
+  };  // namespace reweight
 };  // namespace nuisance
 
 #endif // __GENIE_ENABLED__
