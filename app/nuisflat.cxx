09c0d91267708775d41a6189dc74d5f8450a6928
cwret <c.wret14@imperial.ac.uk>
2018-09-25 17:23:57 +0100

    Allow nuisflat to take card file with weighting parameters

--- app/nuisflat.cxx
+++ app/nuisflat.cxx
@@ -32,6 +32,17 @@ std::string gOptNumberEvents = "NULL";
 std::string gOptCardInput = "";
 std::string gOptOptions = "";
 
+// Input Dial Vals
+std::vector<std::string> fParams; ///< Vector of dial names.
+std::map<std::string, std::string> fStateVals; ///< Map of dial states
+std::map<std::string, double>      fCurVals; ///< Map of dial values
+std::map<std::string, int>         fTypeVals; ///< Map of dial type enums.
+// Fake Dial Vals
+std::map<std::string,double> fFakeVals; ///< Map of fake data settings.
+
+void SetupComparisonsFromXML();
+void SetupRWEngine();
+
 //*******************************
 void PrintSyntax() {
   //*******************************
@@ -152,6 +163,10 @@ int main(int argc, char* argv[]) {
   // Create a new measurementbase class depending on the Format
   MeasurementBase* flattreecreator = NULL;
 
+  SetupComparisonsFromXML();
+  SetupRWEngine();
+  //SetupFCN();
+
   // Make a new sample key for the format of interest.
   nuiskey samplekey = Config::CreateKey("sample");
   if (!gOptFormat.compare("GenericFlux")) {
@@ -184,4 +199,195 @@ int main(int argc, char* argv[]) {
   LOG(FIT) << "-------------------------------------" << std::endl;
 
   return 0;
+}
+//*************************************
+void SetupComparisonsFromXML() {
+  //*************************************
+
+  LOG(FIT) << "Setting up nuiscomp" << std::endl;
+
+  // Setup Parameters ------------------------------------------
+  std::vector<nuiskey> parkeys = Config::QueryKeys("parameter");
+  if (!parkeys.empty()) {
+    LOG(FIT) << "Number of parameters :  " << parkeys.size() << std::endl;
+  }
+
+  for (size_t i = 0; i < parkeys.size(); i++) {
+    nuiskey key = parkeys.at(i);
+
+    // Check for type,name,nom
+    if (!key.Has("type")) {
+      ERR(FTL) << "No type given for parameter " << i << std::endl;
+      ERR(FTL) << "type='PARAMETER_TYPE'" << std::endl;
+      throw;
+    } else if (!key.Has("name")) {
+      ERR(FTL) << "No name given for parameter " << i << std::endl;
+      ERR(FTL) << "name='SAMPLE_NAME'" << std::endl;
+      throw;
+    } else if (!key.Has("nominal")) {
+      ERR(FTL) << "No nominal given for parameter " << i << std::endl;
+      ERR(FTL) << "nominal='NOMINAL_VALUE'" << std::endl;
+      throw;
+    }
+
+    // Get Inputs
+    std::string partype = key.GetS("type");
+    std::string parname = key.GetS("name");
+    double parnom  = key.GetD("nominal");
+    double parlow  = parnom - 1;
+    double parhigh = parnom + 1;
+    double parstep = 1;
+
+    // override if state not given
+    if (!key.Has("state")){
+      key.SetS("state","FIX");
+    }
+
+    std::string parstate = key.GetS("state");
+
+    // Check for incomplete limtis
+    int limdef = ((int)key.Has("low")  +
+        (int)key.Has("high") +
+        (int)key.Has("step"));
+
+    if (limdef > 0 and limdef < 3){
+      ERR(FTL) << "Incomplete limit set given for parameter : " << parname << std::endl;
+      ERR(FTL) << "Requires: low='LOWER_LIMIT' high='UPPER_LIMIT' step='STEP_SIZE' " << std::endl;
+      throw;
+    }
+
+    // Extra limits
+    if (key.Has("low")) {
+
+      parlow  = key.GetD("low");
+      parhigh = key.GetD("high");
+      parstep = key.GetD("step");
+
+      LOG(FIT) << "Read " << partype << " : "
+        << parname << " = "
+        << parnom << " : "
+        << parlow << " < p < " << parhigh
+        << " : " << parstate << std::endl;
+    } else {
+      LOG(FIT) << "Read " << partype << " : "
+        << parname << " = "
+        << parnom << " : "
+        << parstate << std::endl;
+    }
+
+    // Convert if required
+    if (parstate.find("ABS") != std::string::npos) {
+      parnom  = FitBase::RWAbsToSigma( partype, parname, parnom  );
+      parlow  = FitBase::RWAbsToSigma( partype, parname, parlow  );
+      parhigh = FitBase::RWAbsToSigma( partype, parname, parhigh );
+      parstep = FitBase::RWAbsToSigma( partype, parname, parstep );
+    } else if (parstate.find("FRAC") != std::string::npos) {
+      parnom  = FitBase::RWFracToSigma( partype, parname, parnom  );
+      parlow  = FitBase::RWFracToSigma( partype, parname, parlow  );
+      parhigh = FitBase::RWFracToSigma( partype, parname, parhigh );
+      parstep = FitBase::RWFracToSigma( partype, parname, parstep );
+    }
+
+    // Push into vectors
+    fParams.push_back(parname);
+
+    fTypeVals[parname]  = FitBase::ConvDialType(partype);;
+    fCurVals[parname]   = parnom;
+    fStateVals[parname] = parstate;
+
+  }
+
+  // Setup Samples ----------------------------------------------
+  std::vector<nuiskey> samplekeys =  Config::QueryKeys("sample");
+  if (!samplekeys.empty()) {
+    LOG(FIT) << "Number of samples : " << samplekeys.size() << std::endl;
+  }
+
+  for (size_t i = 0; i < samplekeys.size(); i++) {
+    nuiskey key = samplekeys.at(i);
+
+    // Get Sample Options
+    std::string samplename = key.GetS("name");
+    std::string samplefile = key.GetS("input");
+
+    std::string sampletype =
+      key.Has("type") ? key.GetS("type") : "DEFAULT";
+
+    double samplenorm =
+      key.Has("norm") ? key.GetD("norm") : 1.0;
+
+    // Print out
+    LOG(FIT) << "Read Sample " << i << ". : "
+      << samplename << " (" << sampletype << ") [Norm=" << samplenorm<<"]"<< std::endl
+      << "                                -> input='" << samplefile  << "'" << std::endl;
+
+    // If FREE add to parameters otherwise continue
+    if (sampletype.find("FREE") == std::string::npos) {
+      continue;
+    }
+
+    // Form norm dial from samplename + sampletype + "_norm";
+    std::string normname = samplename + "_norm";
+
+    // Check normname not already present
+    if (fTypeVals.find("normname") != fTypeVals.end()) {
+      continue;
+    }
+
+    // Add new norm dial to list if its passed above checks
+    fParams.push_back(normname);
+
+    fTypeVals[normname] = kNORM;
+    fStateVals[normname] = sampletype;
+    fCurVals[normname] = samplenorm;
+
+  }
+
+  // Setup Fake Parameters -----------------------------
+  std::vector<nuiskey> fakekeys = Config::QueryKeys("fakeparameter");
+  if (!fakekeys.empty()) {
+    LOG(FIT) << "Number of fake parameters : " << fakekeys.size() << std::endl;
+  }
+
+  for (size_t i = 0; i < fakekeys.size(); i++) {
+    nuiskey key = fakekeys.at(i);
+
+    // Check for type,name,nom
+    if (!key.Has("name")) {
+      ERR(FTL) << "No name given for fakeparameter " << i << std::endl;
+      throw;
+    } else if (!key.Has("nominal")) {
+      ERR(FTL) << "No nominal given for fakeparameter " << i << std::endl;
+      throw;
+    }
+
+    // Get Inputs
+    std::string parname = key.GetS("name");
+    double parnom  = key.GetD("nominal");
+
+    // Push into vectors
+    fFakeVals[parname] = parnom;
+  }
+}
+
+//*************************************
+void SetupRWEngine() {
+  //*************************************
+
+  LOG(FIT) << "Setting up FitWeight Engine" << std::endl;
+  for (UInt_t i = 0; i < fParams.size(); i++) {
+    std::string name = fParams[i];
+    FitBase::GetRW()->IncludeDial(name, fTypeVals.at(name));
+  }
+
+  for (UInt_t i = 0; i < fParams.size(); i++) {
+    std::string name = fParams[i];
+
+    if (fCurVals.find(name) == fCurVals.end()) continue;
+    FitBase::GetRW()->SetDialValue(name, fCurVals.at(name));
+  }
+
+  FitBase::GetRW()->Reconfigure();
+  return;
+
 }
