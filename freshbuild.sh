a62f6e424189445fff56a032c4eaea60003b66f0
cwret <c.wret14@imperial.ac.uk>
2018-09-29 05:27:03 +0100

    include minerva rw in freshbuild

--- freshbuild.sh
+++ freshbuild.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-
 mkdir build
 cd ./build
 if [[ -e CMakeCache.txt ]]; then
   rm -v CMakeCache.txt
 fi
-cmake -DUSE_NuWro=0 -DUSE_NIWG=0 -DUSE_NEUT=1 -DUSE_GENIE=1 -DUSE_T2K=0 -DUSE_GiBUU=0 -DUSE_NUANCE=0 -DUSE_MINIMIZER=false ../
+cmake -DUSE_NuWro=0 -DUSE_NIWG=0 -DUSE_MINERvA_RW=1 -DUSE_NEUT=0 -DUSE_GENIE=1 -DUSE_T2K=0 -DUSE_GiBUU=0 -DUSE_NUANCE=0 -DUSE_MINIMIZER=false ../
 make clean && make -j8
 cd -
