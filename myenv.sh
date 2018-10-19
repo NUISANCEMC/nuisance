57aa6971e8a631248d35d64c4707b455613e423b
cwret <c.wret14@imperial.ac.uk>
2018-10-19 14:44:04 +0100

    Adding missing myenv example file

---
+++ myenv.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#############
+## ROOT RELATED BUSINESS
+source /vols/t2k/users/cvw09/software/root/bin/thisroot.sh
+
+
+
+#############
+## NEUT RELATED BUSINESS
+#export NEUT_ROOT=/vols/t2k/users/cvw09/software/neut-devel/trunk
+#export NEUT_ROOT=~/ssd/neut-devel/branches/neut_5.4.0.1
+#export NEUT_VERSION=5.4.0.1
+#export NEUT_VERSION=5.4.0.1
+
+#export NEUT_ROOT=/vols/build/t2k/cvw09/neut-devel/branches/neut_5.4.1_alpha
+#export NEUT_VERSION=5.4.1
+#export NEUT_ROOT=~/ssd/neut_5.3.6_minoo
+#export CERN=~/ssd/CERNLIB
+#export CERN_LEVEL=2005
+#export LD_LIBRARY_PATH=${NEUT_ROOT}/src/reweight:${LD_LIBRARY_PATH}
+
+#############
+## GENIE RELATED BUSINESS
+#source ~/ssd/lamp/environment_setup.sh
+#source ~/ssd/lamp/environment_setup_2.8.4.sh
+#source ~/ssd/lamp/environment_setup_2.10.10.sh
+
+export NUISANCE=$(pwd -P)
