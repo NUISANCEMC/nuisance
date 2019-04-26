4c14e1ac575606aad0d8122e7f09a26ddc511ae3
cwret <c.wret14@imperial.ac.uk>
2019-04-26 23:05:44 +0100

    Change single pion W cut to 1.8

--- src/MINERvA/MINERvA_SignalDef.cxx
+++ src/MINERvA/MINERvA_SignalDef.cxx
@@ -130,7 +130,8 @@ namespace SignalDef {
 
     // Extract Hadronic Mass
     double hadMass = FitUtils::Wrec(pnu, pmu);
-    if (hadMass > 1400.0) return false;
+    // Cut on 2017 data is 1.8 GeV
+    if (hadMass > 1800.0) return false;
 
     return true;
   };
