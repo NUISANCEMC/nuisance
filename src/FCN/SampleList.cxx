b74d7851ba017021b6e6a28c3b1f8e08a4bcf502
Ciro Riccio <ciro.riccio@cern.ch>
2020-07-01 17:23:44 +0200

    Fix

--- src/FCN/SampleList.cxx
+++ src/FCN/SampleList.cxx
@@ -1263,8 +1263,8 @@ MeasurementBase *CreateSample(nuiskey samplekey) {
   } else if (!name.compare("T2K_AntiNuMu_CC0pi_CH_XSec_2DPcos")) {
     return (new T2K_AntiNuMu_CC0pi_CH_XSec_2DPcos(samplekey));
 
-  } else if (!name.compare("T2K_NuMu_CC0pi_CH_XSec_2DPcos" ||
-             !name.compare("T2K_AntiNuMu_CC0pi_CH_XSec_2DPcos" )) {
+  } else if (!name.compare("T2K_NuMu_CC0pi_CH_XSec_2DPcos") ||
+             !name.compare("T2K_AntiNuMu_CC0pi_CH_XSec_2DPcos")) {
     return (new T2K_NuMuAntiNuMu_CC0pi_CH_XSec_2DPcos(samplekey));
 
   } else if (!name.compare("T2K_nueCCinc_XSec_1Dpe_FHC") ||
