a4eb6e46c886484f591f904ebd808874af7624da
Ciro Riccio <ciro.riccio@cern.ch>
2020-07-05 16:57:09 +0200

    Test the new changes on the server

--- src/T2K/T2K_NuMuAntiNuMu_CC0pi_XSec_joint.h
+++ src/T2K/T2K_NuMuAntiNuMu_CC0pi_XSec_joint.h
@@ -22,11 +22,11 @@ public:
   T2K_NuMuAntiNuMu_CC0pi_CH_XSec_2DPcos* AntiNuMuCC0pi;
 
   bool isSignal(){return false;};
-  void SetHistograms(){return;};
   void FillEventVariables(){return;};
-  void FillHistograms(){return;};
-  void ConvertEventRates(){return;};
-  void ScaleEvents(){return;};
+  T2K_NuMuAntiNuMu_CC0pi_CH_XSec_2DPcos::SetHistograms();
+  T2K_NuMuAntiNuMu_CC0pi_CH_XSec_2DPcos::FillHistograms();
+  T2K_NuMuAntiNuMu_CC0pi_CH_XSec_2DPcos::ConvertEventRates();
+  T2K_NuMuAntiNuMu_CC0pi_CH_XSec_2DPcos::ScaleEvents();
 
 };
 
