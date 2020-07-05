828cc2c78cd2e06f0d5e65b6a1e9d4fe3fe3e199
Ciro Riccio <ciro.riccio@cern.ch>
2020-07-05 21:03:32 +0200

    Additional test to make it working

--- src/T2K/T2K_NuMuAntiNuMu_CC0pi_XSec_joint.h
+++ src/T2K/T2K_NuMuAntiNuMu_CC0pi_XSec_joint.h
@@ -9,13 +9,13 @@ public:
 
   T2K_NuMuAntiNuMu_CC0pi_XSec_joint(nuiskey samplekey);
   virtual ~T2K_NuMuAntiNuMu_CC0pi_XSec_joint() {};
-
-  // Makes a data hist from all the smaller ones
-  void CombineDataHists();
   
   // Makes a MC hist from all the smaller ones
   void MakePlots();
-  
+
+  // Makes a data hist from all the smaller ones
+  void CombineDataHists();
+
   void ScaleEvents(){return;};
   bool isSignal(){return false;};
   void FillEventVariables(){return;};
@@ -27,11 +27,10 @@ public:
   /// Event scaling 
   void ConvertEventRates(){return;};
  
-
  private:
   // The separate measurements that go into this
-  T2K_NuMuAntiNuMu_CC0pi_CH_XSec_2DPcos* NuMuCC0pi;
-  T2K_NuMuAntiNuMu_CC0pi_CH_XSec_2DPcos* AntiNuMuCC0pi;
+  Measurement1D* NuMuCC0pi;
+  Measurement1D* AntiNuMuCC0pi;
 
 };
 
