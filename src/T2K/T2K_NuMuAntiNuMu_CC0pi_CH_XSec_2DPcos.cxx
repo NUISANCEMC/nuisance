fe020a30aa63b4e2472d51991b87a01fd3043781
Ciro Riccio <ciro.riccio@cern.ch>
2020-07-05 21:23:58 +0200

    Test it this works

--- src/T2K/T2K_NuMuAntiNuMu_CC0pi_CH_XSec_2DPcos.cxx
+++ src/T2K/T2K_NuMuAntiNuMu_CC0pi_CH_XSec_2DPcos.cxx
@@ -43,7 +43,6 @@ T2K_NuMuAntiNuMu_CC0pi_CH_XSec_2DPcos::T2K_NuMuAntiNuMu_CC0pi_CH_XSec_2DPcos(nui
                     "arXiv:2002.09323";
     fSettings.SetTitle("T2K_NuMu_CC0pi_CH_XSec_2DPcos");
     fSettings.DefineAllowedSpecies("numu");
-    fSettings.SetDataInput(FitPar::GetDataBase() + "/T2K/CC0pi/JointNuMu-AntiNuMu/JointNuMuAntiNuMuCC0piXsecDataRelease.root"); 
     NuPDG  = 14;
     LepPDG = 13;
   } 
@@ -56,7 +55,7 @@ T2K_NuMuAntiNuMu_CC0pi_CH_XSec_2DPcos::T2K_NuMuAntiNuMu_CC0pi_CH_XSec_2DPcos(nui
 
     fSettings.SetTitle("T2K_AntiNuMu_CC0pi_CH_XSec_2DPcos");
     fSettings.DefineAllowedSpecies("numub");
-    fSettings.SetDataInput(FitPar::GetDataBase() + "/T2K/CC0pi/JointNuMu-AntiNuMu/JointNuMuAntiNuMuCC0piXsecDataRelease.root"); 
+
     NuPDG  = -14;
     LepPDG = -13;
   }
@@ -67,6 +66,7 @@ T2K_NuMuAntiNuMu_CC0pi_CH_XSec_2DPcos::T2K_NuMuAntiNuMu_CC0pi_CH_XSec_2DPcos(nui
   fSettings.SetAllowedTypes("DIAG,FULL/FREE,SHAPE,FIX/SYSTCOV/STATCOV","FIX");
   fSettings.SetEnuRangeFromFlux(fFluxHist);
   fSettings.DefineAllowedTargets("C,H");
+  fSettings.SetDataInput(FitPar::GetDataBase() + "/T2K/CC0pi/JointNuMu-AntiNuMu/JointNuMuAntiNuMuCC0piXsecDataRelease.root"); 
   FinaliseSampleSettings();
 
   // Scaling Setup ---------------------------------------------------
