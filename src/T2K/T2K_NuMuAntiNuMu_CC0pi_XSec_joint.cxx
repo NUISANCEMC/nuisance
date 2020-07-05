f584b0c65df7b6e2e6858a312e903a809beffce7
Ciro Riccio <ciro.riccio@cern.ch>
2020-07-05 17:22:08 +0200

    Fix

--- src/T2K/T2K_NuMuAntiNuMu_CC0pi_XSec_joint.cxx
+++ src/T2K/T2K_NuMuAntiNuMu_CC0pi_XSec_joint.cxx
@@ -65,12 +65,10 @@ T2K_NuMuAntiNuMu_CC0pi_XSec_joint::T2K_NuMuAntiNuMu_CC0pi_XSec_joint(nuiskey sam
 void T2K_NuMuAntiNuMu_CC0pi_XSec_joint::test() {
 //********************************************************************
 
-  NuMuCC0pi->FillEventVariables();
   NuMuCC0pi->SetHistograms();
   NuMuCC0pi->FillHistograms();
   NuMuCC0pi->ConvertEventRates();
 
-  AntiNuMuCC0pi->FillEventVariables();
   AntiNuMuCC0pi->SetHistograms();
   AntiNuMuCC0pi->FillHistograms();
   AntiNuMuCC0pi->ConvertEventRates();
