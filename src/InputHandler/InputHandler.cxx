0dc6c1a55ff81ae47e4b8b25b28278f17b1a1598
wilkinson-nu <cwilkinson@lbl.gov>
2020-11-17 22:03:50 +0100

    Remove redundant warning (GiBUUInputHandler overrides this method)

--- src/InputHandler/InputHandler.cxx
+++ src/InputHandler/InputHandler.cxx
@@ -247,14 +247,6 @@ void InputHandlerBase::SetupJointInputs() {
     NUIS_ABORT("Can only handle joint inputs when config MAXEVENTS = -1!");
   }
 
-  if (jointeventinputs.size() > 1) {
-    NUIS_ERR(
-        WRN,
-        "GiBUU sample contains multiple inputs. This will only work for "
-        "samples that expect multi-species inputs. If this sample does, you "
-        "can ignore this warning.");
-  }
-
   for (size_t i = 0; i < jointeventinputs.size(); i++) {
     double scale = double(fNEvents) / fEventHist->Integral("width");
     scale *= jointeventinputs.at(i)->Integral("width");
