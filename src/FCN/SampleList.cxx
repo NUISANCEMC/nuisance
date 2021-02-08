08651dff442ac4b6247e35185a22c8ad10ba2928
wilkinson-nu <cwilkinson@lbl.gov>
2021-02-09 00:35:41 +0100

    Fix a second erroneous sample name comes out for nuissamples

--- src/FCN/SampleList.cxx
+++ src/FCN/SampleList.cxx
@@ -1199,8 +1199,7 @@ MeasurementBase *CreateSample(nuiskey samplekey) {
              !name.compare("MINERvA_CCNpip_XSec_1DTpi_nu_2016") ||
              !name.compare("MINERvA_CCNpip_XSec_1DTpi_nu_2015_20deg") ||
              !name.compare("MINERvA_CCNpip_XSec_1DTpi_nu_2015_fluxcorr") ||
-             !name.compare(
-                 "MINERvA_CCNpip_XSec_1DTpi_nu_2015_20deg_fluxcorr")) {
+             !name.compare("MINERvA_CCNpip_XSec_1DTpi_nu_2015_20deg_fluxcorr")) {
     return (new MINERvA_CCNpip_XSec_1DTpi_nu(samplekey));
 
   } else if (!name.compare("MINERvA_CCNpip_XSec_1Dthmu_nu")) {
